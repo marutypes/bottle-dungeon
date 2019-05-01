bba9c3c8886d3ef586575125a9ebd1da0f47810c
Mathew Allen <mathew.allen@shopify.com>
2019-04-30 22:26:51 -0400

    authentication plug

--- lib/bottle_dungeon_web/router.ex
+++ lib/bottle_dungeon_web/router.ex
@@ -7,6 +7,7 @@ defmodule BottleDungeonWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug BottleDungeonWeb.Auth
   end
 
   pipeline :api do
